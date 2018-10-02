a198e988fe73f0982627650d8375c815d7abbfdc
psahu6 <prafful.sahu1@gmail.com>
2018-10-02 22:27:48 +0530

    PDF integration sample

---
+++ src/components/PDF.vue
@@ -0,0 +1,31 @@
+<template>
+    <!-- <h1>Hello PDF</h1> -->
+    <v-app>
+    <p>Hello PDF</p>
+    <!-- <div id="disqus_thread"></div>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript> -->
+    </v-app>        
+</template>
+
+<script>
+export default {
+name: "PDF",
+
+mounted(){
+    (function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://allaboutai.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+// disqus_shortname = 'allaboutai';
+        // jQuery(document).ready(function() {
+        //   jQuery("p").inlineDisqussions();
+        // });
+}
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import MainPage from '@/components/MainPage'
+import PDF from '@/components/PDF'
+import PDF2 from '@/components/PDF2'
 import Item from '@/components/Item'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
@@ -15,6 +17,16 @@ export default new Router({
       path: '/',
       name: 'MainPage',
       component: MainPage
+    },
+    {
+      path:'/pdfsample',
+      name: 'PDF',
+      component:PDF
+    },
+    {
+      path:'/pdf2',
+      name: 'PDF2',
+      component:PDF2
     }
   ]
 })
